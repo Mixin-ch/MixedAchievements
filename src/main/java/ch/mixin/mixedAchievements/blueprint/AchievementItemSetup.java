8b12a36b89dc601c2dd5043932d734d2020dd478
Dokzentir <mischa.iwasaki@gmail.com>
2021-05-28 19:47:18 +0200

    loreList

--- src/main/java/ch/mixin/mixedAchievements/blueprint/AchievementItemSetup.java
+++ src/main/java/ch/mixin/mixedAchievements/blueprint/AchievementItemSetup.java
@@ -2,21 +2,27 @@ package ch.mixin.mixedAchievements.blueprint;
 
 import org.bukkit.Material;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AchievementItemSetup {
     private Material material;
     private String name;
     private int amount;
+    private List<String> loreList;
 
-    public AchievementItemSetup(Material material, String name, int amount) {
+    public AchievementItemSetup(Material material, String name, int amount, List<String> loreList) {
         this.material = material;
         this.name = name;
         this.amount = amount;
+        this.loreList = loreList;
     }
 
     public AchievementItemSetup() {
         this.material = Material.AIR;
         this.name = "";
         this.amount = 0;
+        loreList = new ArrayList<>();
     }
 
     public Material getMaterial() {
@@ -41,5 +47,13 @@ public class AchievementItemSetup {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    public List<String> getLoreList() {
+        return loreList;
+    }
+
+    public void setLoreList(List<String> loreList) {
+        this.loreList = loreList;
     }
 }
